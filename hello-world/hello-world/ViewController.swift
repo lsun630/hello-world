ce88ea515c93d7f58a81bf82f24bed3b94595d87
lsun630 <louissun630@yahoo.com>
2017-09-10 16:49:44 -0400

    Add files via upload

--- hello-world/hello-world/ViewController.swift
+++ hello-world/hello-world/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
     @IBOutlet var label1: UILabel!
+    @IBOutlet weak var textField1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
